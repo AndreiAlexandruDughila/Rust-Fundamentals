31da84c8adb5ee06d34c6932accf3f9198e12b2a
AndreiAlexandruDughila <andreialexandru.d11@gmail.com>
2021-06-19 23:21:34 +0300

    Program No 2

---
+++ p002/src/main.rs
@@ -0,0 +1,40 @@
+use std::mem;
+#[allow(unused_variables)]
+fn main() {
+    println!("Hello, world!");
+
+    let mut x : u32 = 5;
+    let y : isize = 3;
+    x = 11;
+    println!("{}", x);
+    println!("{}", mem::size_of::<* const isize>());
+
+
+    let heap_vector: Vec<i8> = Vec::new();
+    let heap_vector1: Vec<u8> = vec![5,2];
+    let heap_vector3 = heap_vector1.clone();
+
+    println!("{}", heap_vector1[1]);
+    println!("{}", heap_vector3[1]);
+    println!("{:?}", heap_vector);
+
+    let stack_f64: f64 = 1.;
+    let heap_f64: Box<f64> = Box::new(2.);
+
+    stack_procedure(stack_f64);
+    heap_procedure(&heap_f64);
+
+    println!("{}", heap_f64);
+
+}
+
+
+fn stack_procedure(mut param: f64){
+    param +=9.;
+    println!("Stack here: {}", param);
+}
+
+fn heap_procedure(param: &Box<f64>){
+    println!("Heap here: {}", param);
+}
+
